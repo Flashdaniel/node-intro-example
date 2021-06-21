e35a5629f41ca0e07fe38b488c27cd21a20bd395
Flashdaniel <49584202+Flashdaniel@users.noreply.github.com>
2021-06-21 12:57:30 +0100

    First commit

---
+++ public/complements.js
@@ -0,0 +1,15 @@
+/** @format */
+document
+	.querySelector(".request-complement")
+	.addEventListener("click", function () {
+		fetch("/complement")
+			.then(function (res) {
+				return res.json();
+			})
+			.then(function (data) {
+				document.querySelector(".complement").innerText = data.complement;
+			})
+			.catch(function (err) {
+				console.error(err);
+			});
+	});
